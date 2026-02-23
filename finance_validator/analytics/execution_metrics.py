ae13fccc7d5cde64dabe29f68be5f140a84823a1
Krisdr00m <kristophe.yen1@gmail.com>
2026-02-22 19:32:28 -0500

    feat: function for volume by symbol

--- finance_validator/analytics/execution_metrics.py
+++ finance_validator/analytics/execution_metrics.py
@@ -1,12 +1,14 @@
 from trade_parser.models import Trade
 
 def volume_by_symbol(trades: list[Trade]) -> dict[str, int]:
-    symbol_dict = {}
+    symbol_dict = dict()
     for trade in trades:
-        if symbol_dict[trade.symbol]:
-            symbol_dict[trade.symbol] += trade.quantity
-        else:
+        try:
+            if(symbol_dict[trade.symbol]):
+                symbol_dict[trade.symbol] += trade.quantity
+        except(KeyError):
             symbol_dict[trade.symbol] = trade.quantity
+            
     
     return symbol_dict
             
