017e5beae942a483c973c5df146417014fb0a61f
Krisdr00m <kristophe.yen1@gmail.com>
2026-02-22 20:11:20 -0500

    feat: function for vwap

--- finance_validator/analytics/execution_metrics.py
+++ finance_validator/analytics/execution_metrics.py
@@ -11,4 +11,21 @@ def volume_by_symbol(trades: list[Trade]) -> dict[str, int]:
             
     
     return symbol_dict
-            
+
+def vwap_by_symbol(trades: list[Trade]) -> dict[str, float]:
+    
+    price_quantity_factor_dict = dict()
+    volume_dict = volume_by_symbol(trades)
+    vwap_by_symbol = dict()
+    
+    for trade in trades:
+        try:
+            if(price_quantity_factor_dict[trade.symbol]):
+                price_quantity_factor_dict[trade.symbol] += trade.quantity * trade.price
+        except(KeyError):
+            price_quantity_factor_dict[trade.symbol] = trade.quantity * trade.price
+    
+    for key, value in price_quantity_factor_dict.items():
+        vwap_by_symbol[key] = value/volume_dict[key]
+    
+    return vwap_by_symbol
